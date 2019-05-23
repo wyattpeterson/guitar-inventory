ab4a0bde7124490f3c55ce506764957516bb407d
Wyatt Peterson <wpeterson@nerdwallet.com>
2019-05-22 20:13:26 -0700

    wip on adding UI

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 const app = express();
-const port = 8081; // default port to listen
+const port = 8080; // default port to listen
 
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
